063eadef7c757c42a57c80086adc4de624377784
Jason Bourne <jbourne@Jasons-MacBook.local>
2018-10-01 20:35:10 -0400

    completed first part of the lab

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+  public static void main(String[] args) {
+    int x = 0;
+    int y = 0;
+    while (x < 10) {
+      y = x + y;
+      System.out.print(y + " ");
+      System.out.println(5 + 10);
+      x = x + 1;
+    }
+  }
+}
